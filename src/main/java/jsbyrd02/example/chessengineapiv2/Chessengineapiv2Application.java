469a693ccbaa4f7426b3cad6ff3e8fb15fbb1e91
Joshua <jsbyrd02@gmail.com>
2023-09-11 17:13:12 -0400

    Initial commit

---
+++ src/main/java/jsbyrd02/example/chessengineapiv2/Chessengineapiv2Application.java
@@ -0,0 +1,13 @@
+package jsbyrd02.example.chessengineapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chessengineapiv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chessengineapiv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/jsbyrd02/example/chessengineapiv2/Chessengineapiv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jsbyrd02.example.chessengineapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chessengineapiv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
